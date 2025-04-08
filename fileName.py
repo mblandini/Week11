b54977743412b5d5c43b44185a5236307d6d656d
Madeline Blandini <madelineblandini@Madelines-MacBook-Pro-4.local>
2025-04-08 19:48:47 -0400

    Level 7 Complete

--- fileName.py
+++ fileName.py
@@ -29,3 +29,50 @@ $$$Variable = "bad"
 x = type (3) 
 y = type ("Hello")
 z = type (True)
+
+print(x)
+print(y)
+print(z)
+
+#Put the Data Type in the comment
+my_integer = 42               #Integer
+my_integer_type = type(my_integer)
+print(my_integer_type)        
+
+my_float = 3.14               #Float
+my_float_type = type(my_float)
+print(my_float_type)          
+
+my_string = "Hello, World"    #String
+my_string_type = type(my_string)
+print(my_string_type)
+
+my_boolean_true = True        #Boolean true
+my_boolean_true_type = type(my_boolean_true)
+print(my_boolean_true_type)
+
+my_boolean_false = False      #Boolean false
+my_boolean_false_type = type(my_boolean_false)
+print(my_boolean_false_type)
+
+my_none_value = None          #None value
+my_none_value_type = type(my_none_value)
+print(my_none_value_type)
+
+my_negative_integer = -7      #Negative integer
+my_negative_integer_type = type(my_negative_integer)
+print(my_negative_integer_type)
+
+my_large_float = 1.2345678e10 #Large float
+my_large_float_type = type(my_large_float)
+print(my_large_float_type)
+
+my_single_character = 'A'     #Single character 
+my_single_character_type = type(my_single_character)
+print(my_single_character_type)
+
+
+my_empty_string = ""          #Empty string
+my_empty_string_type = type(my_empty_string)
+print(my_empty_string_type)
+
