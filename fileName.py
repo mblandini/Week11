cf554d3e238d3caba64c7badd7f65e4057d2d165
Madeline Blandini <madelineblandini@Madelines-MacBook-Pro-4.local>
2025-04-08 19:14:09 -0400

    Level 4 Complete

--- fileName.py
+++ fileName.py
@@ -1,3 +1,14 @@
 # Name : Madeline Blandini
 
 x = None 
+
+example = "I'm a string" # The variable called example is now holding the string value "I'm a string"
+a = 3 # The variable is now called a is now holding the integer value 3
+b = 4.0 # The variable called b is now holding the float value 
+c = True # The variable now called c is now holding the boolean value True
+d = False # The variable now called d is now holding the boolean value False
+e = "hey" # The variable now called e is now holding the string value "hey"
+f = None # The variable now called f is now holding the None value (absence of a value)
+age = 32 # The variable now called age is now holding the integer value 3
+name = "Insert your name here" # The variable now called name is holding the string value "Insert your name here"
+instrument = "Insert your name here" # The variable now called instrument is holding the string value "Insert your instrument name here"
