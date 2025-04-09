8d06cbb679e88fdd2e88250be7e0832422e4f080
Madeline Blandini <madelineblandini@Madelines-MacBook-Pro-4.local>
2025-04-08 20:14:48 -0400

    Level 8 Complete

---
+++ level_8.py
@@ -0,0 +1,12 @@
+x = input() #No Prompt
+print('Hello, ' + x)
+
+pass_variable = "Enter your Name"
+x = input (pass_variable)
+print('Hello, ' + x)
+
+# You can't pass a non-string variable to input BUT you can pass a variable containing a string
+
+
+
+
